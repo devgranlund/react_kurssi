539e320bcd01719560b5f773a49f4e7a213d20fc
tuomas.granlund <tuomas.granlund@mylab.fi>
2018-04-07 14:13:20 +0300

    Tehtävä 1.2

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -13,9 +13,8 @@ const App = () => {
     return (
         <div>
             <Otsikko teksti={kurssi} />
-            <Sisalto osa={osa1} tehtavia={tehtavia1} />
-            <Sisalto osa={osa2} tehtavia={tehtavia2} />
-            <Sisalto osa={osa3} tehtavia={tehtavia3} />
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} 
+                     tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
             <Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3} />
         </div>
     )
@@ -28,6 +27,16 @@ const Otsikko = (props) => {
 }
 
 const Sisalto = (props) => {
+    return (
+        <div>
+            <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+            <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+            <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
+        </div>
+    )
+}
+
+const Osa = (props) => {
     return (
         <p>{props.osa} {props.tehtavia}</p>
     )
