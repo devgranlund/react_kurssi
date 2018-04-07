d6d6fe00892e76350a662430e8d9b57dc001150c
tuomas.granlund <tuomas.granlund@mylab.fi>
2018-04-07 13:54:56 +0300

    Tehtävä 1.1

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = 'Reactin perusteet'
+    const tehtavia1 = 10
+    const osa2 = 'Tiedonvälitys propseilla'
+    const tehtavia2 = 7
+    const osa3 = 'Komponenttien tila'
+    const tehtavia3 = 14
+
+    return (
+        <div>
+            <Otsikko teksti={kurssi} />
+            <Sisalto osa={osa1} tehtavia={tehtavia1} />
+            <Sisalto osa={osa2} tehtavia={tehtavia2} />
+            <Sisalto osa={osa3} tehtavia={tehtavia3} />
+            <Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3} />
+        </div>
+    )
+}
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.teksti}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <p>{props.osa} {props.tehtavia}</p>
+    )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <p>yhteensa {props.tehtavia} tehtävää</p>   
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
