0eb6db6552f41db090168fad5c64bdcf6a1e49e5
tuomas.granlund <tuomas.granlund@mylab.fi>
2018-04-18 23:05:29 +0300

    Tehtävä 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -12,7 +12,19 @@ const totalLikes = (blogs) => {
     }
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length > 0){
+        const max = blogs
+            .map(blog => blog.likes)
+            .reduce((acc, cur) => Math.max(acc, cur))
+        return blogs.filter(blog => blog.likes === max)[0]
+    } else {
+        return {}
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
