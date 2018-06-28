4d24bc6cce5c6817df7d4a23aab479167b2ed0fd
tuomas.granlund <tuomas.granlund@mylab.fi>
2018-06-28 22:38:40 +0300

    Tethtävä 5.1

---
+++ osa5/.eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+    "env": {
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended", 
+        "plugin:react/recommended"
+    ],
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+                                               // default to "createReactClass"
+            "pragma": "React",  // Pragma to use, default to "React"
+            "version": "15.0", // React version, default to the latest React stable release
+            "flowVersion": "0.53" // Flow version
+        },
+        "propWrapperFunctions": [ "forbidExtraProps" ] // The names of any functions used to wrap the
+                                                       // propTypes object, e.g. `forbidExtraProps`.
+                                                       // If this isn't set, any propTypes wrapped in
+                                                       // a function will be skipped.
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0,
+        "no-unused-vars":0,
+        "no-trailing-spaces":0
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "globals": {
+        "test": true,
+        "expect": true,
+        "describe": true
+    }
+};
--- osa5/src/App.js
+++ osa5/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from "./components/Notification";
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+        error: null,
+        username: '',
+        password: '',
+        user: null,
+        blogs: []
     }
   }
 
@@ -14,12 +20,62 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
-  } 
+  }
+  
+  onFieldChange = (event) => {
+      this.setState({ [event.target.name]: event.target.value })
+  }
+  
+  login = async (event) => {
+      event.preventDefault()
+      try {
+          const user = await loginService.login({
+              username: this.state.username,
+              password: this.state.password
+          })
+
+          this.setState({ username: '', password: '', user})
+      } catch (exception) {
+          this.setState({
+              error: 'käyttäjätunnus tai salasana virheellinen',
+          })
+          setTimeout(() => {
+              this.setState({ error: null })
+          }, 5000)
+      }
+  }
 
   render() {
+    if (this.state.user === null){
+        return (
+          <div>
+              <h2>Log in to application</h2>
+              <form onSubmit={this.login}>
+                username:
+                  <input 
+                      type="text"
+                      name="username"
+                      value={this.state.username} 
+                      onChange={this.onFieldChange}/><br/>
+                password:
+                  <input 
+                      type="password"
+                      name="password"
+                      value={this.state.password} 
+                      onChange={this.onFieldChange}/><br/>
+                  <button>login</button>  
+              </form>
+              <Notification 
+                  message={this.state.error}
+                  cssClass={'error'} />
+          </div>  
+        );
+    }
+      // else
     return (
       <div>
         <h2>blogs</h2>
+          {this.state.user.name} logged in. <br/><br/>
         {this.state.blogs.map(blog => 
           <Blog key={blog._id} blog={blog}/>
         )}
