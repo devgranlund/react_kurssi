655089e42db6945d16e74332bca2ac960606bf01
tuomas.granlund <tuomas.granlund@mylab.fi>
2018-12-02 21:28:37 +0200

    Tehtävä 5.16

--- osa5/src/App.test.js
+++ osa5/src/App.test.js
@@ -7,14 +7,37 @@ import blogService from './services/blogs'
 
 describe('<App/>', () => {
     let app
-    beforeAll(() => {
-        app = mount(<App />)
-    })
     
-    it('blogs are not visible before logging in', () => {
-        app.update()
-        const title = app.find('h2')
-        expect(title.text()).toContain("Log in to application")
+    describe('when user is not logged in', () => {
+        beforeEach(() => {
+            app = mount(<App />)
+        })
+        
+        it('blogs are not visible before logging in', () => {
+            app.update()
+            const title = app.find('h2')
+            expect(title.text()).toContain("Log in to application")
+        })
+    })
+
+    describe('when user is logged in', () => {
+        beforeEach(() => {
+            const user = {
+                username: 'tester',
+                token: '1231231214',
+                name: 'Teuvo Testaaja'
+            }
+            localStorage.setItem('authorizedUser', JSON.stringify(user))
+            app = mount(<App />)
+        })
+        
+        it('blogs are visible when user is logged in', () => {
+            app.update()
+            const title = app.find('h2')
+            expect(title.text()).toContain("blogs")
+
+        })
+
     })
     
 })
