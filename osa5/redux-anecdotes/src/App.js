c0e683e99102ccb336620a061e6e773c198ce28a
tuomas.granlund <tuomas.granlund@mylab.fi>
2018-12-04 10:26:24 +0200

    Tehtävä 5.21

--- osa5/redux-anecdotes/src/App.js
+++ osa5/redux-anecdotes/src/App.js
@@ -1,31 +1,41 @@
 import React from 'react';
 
 class App extends React.Component {
-    
-  render() {
-    const anecdotes = this.props.store.getState()
-    return (
-      <div>
-        <h2>Anecdotes</h2>
-        {anecdotes.map(anecdote=>
-          <div key={anecdote.id}>
+
+    addAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        this.props.store.dispatch({type: 'NEW_ANECDOTE', data: content})
+        event.target.anecdote.value = ''
+    }
+
+    render() {
+        const anecdotes = this.props.store.getState()
+        return (
             <div>
-              {anecdote.content} 
+                <h2>Anecdotes</h2>
+                {anecdotes.map(anecdote =>
+                    <div key={anecdote.id}>
+                        <div>
+                            {anecdote.content}
+                        </div>
+                        <div>
+                            has {anecdote.votes}
+                            <button onClick={() => {
+                                this.props.store.dispatch({type: 'VOTE', data: anecdote.id})
+                            }}>vote
+                            </button>
+                        </div>
+                    </div>
+                )}
+                <h2>create new</h2>
+                <form onSubmit={this.addAnecdote}>
+                    <div><input name="anecdote"/></div>
+                    <button type="submit">create</button>
+                </form>
             </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => {this.props.store.dispatch({ type: 'VOTE', data: anecdote.id })}}>vote</button>
-            </div>
-          </div>
-        )}
-        <h2>create new</h2>
-        <form>
-          <div><input /></div>
-          <button>create</button> 
-        </form>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default App
--- osa5/redux-anecdotes/src/reducer.js
+++ osa5/redux-anecdotes/src/reducer.js
@@ -1,47 +1,50 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-const getId = () => (100000*Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
+    return {
+        content: anecdote,
+        id: getId(),
+        votes: 0
+    }
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  let newState
-    if (action.type === 'VOTE'){
-      for (let i = 0; i < state.length; i++){
-        if (state[i].id.toString() === action.data) {
-          const edited = {content: state[i].content, id: state[i].id, votes: state[i].votes + 1}
-          newState = [...state]
-          newState[i] = edited
-          state = newState
+    let newState
+    if (action.type === 'VOTE') {
+        for (let i = 0; i < state.length; i++) {
+            if (state[i].id.toString() === action.data) {
+                const edited = {content: state[i].content, id: state[i].id, votes: state[i].votes + 1}
+                newState = [...state]
+                newState[i] = edited
+                state = newState
+            }
         }
-      }
-    } 
-  return state.sort(compare)
+    } else if (action.type === 'NEW_ANECDOTE') {
+        newState = [...state, asObject(action.data)]
+        state = newState
+    }
+    return state.sort(compare)
 }
 
-const compare = (a,b) => {
-  if (a.votes < b.votes){
-    return 1;
-  } 
-  if (a.votes > b.votes){
-    return -1;
-  }
-  return 0;
+const compare = (a, b) => {
+    if (a.votes < b.votes) {
+        return 1;
+    }
+    if (a.votes > b.votes) {
+        return -1;
+    }
+    return 0;
 }
 
 export default reducer
